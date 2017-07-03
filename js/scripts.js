4f4495150053e640fd6fe76525bd669dd8ee3bf1
LAPTOP-57J009DG\piotr <kubus-o_2@tlen.pl>
2017-07-03 14:10:25 +0200

    Inicjuję respozytorjiu Zadanie_8_4

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+/* Wyskakujące okienko prompt */
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+//  pojawi się informacja w consoli w przeglądarce
+console.log('Hello, ' + name);
